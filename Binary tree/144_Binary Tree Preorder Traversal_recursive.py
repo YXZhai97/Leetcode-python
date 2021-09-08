722d82d3a16f43e51859b7596ba2ddb6fae41e4e
Eason <eason.zhai97@outlook.com>
2021-09-08 18:48:51 +0200

    binary tree

---
+++ Binary tree/144_Binary Tree Preorder Traversal_recursive.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep  8 18:16:27 2021
+
+@author: yixing
+"""
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        if root is None:
+            return []
+        return [root.val]+self.preorderTraversal(root.left)+self.preorderTraversal(root.right)
