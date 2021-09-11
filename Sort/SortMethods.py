351a0c93f12436b3e7ed73d8e7845d6ab9d24544
Eason <eason.zhai97@outlook.com>
2021-09-11 15:33:33 +0200

    bubble sort

--- Sort/SortMethods.py
+++ Sort/SortMethods.py
@@ -25,13 +25,26 @@ insertionSort(array)
     
 
 
-#2. 冒泡排序o
+#2. 冒泡排序
+
+def bubbleSort(arr):
+    n=len(arr)
+    for i in range(n):
+        for j in range(n-1-i): #最后i个已经排好了
+            if arr[j]>arr[j+1]:
+                arr[j],arr[j+1]=arr[j+1],arr[j]
+    print(arr)
+    return arr
+
+array=[1,25,8,2,5,33,10,0,25,100]
+bubbleSort(array)
+
+
+#3. Merge 排序(归并排序)
 
 
 
 
-
-#3. Merge 排序
 
 
 
