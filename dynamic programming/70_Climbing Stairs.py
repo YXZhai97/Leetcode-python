207a64e18c6c38c88995472ee0832830454a0fb7
Eason <eason.zhai97@outlook.com>
2021-09-12 22:53:07 +0200

    70_Climbing Stairs

---
+++ dynamic programming/70_Climbing Stairs.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep 12 22:51:55 2021
+
+@author: yixing
+"""
+
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        #0 1 2 3 4 5 6 7 8
+        #  1 2 3 5
+        
+        if n<=2:
+            return n
+        pre1=1
+        pre2=2
+        
+        for i in range(2,n):
+            current=pre1+pre2
+            pre1=pre2
+            pre2=current
+        
+        return current 
+    
